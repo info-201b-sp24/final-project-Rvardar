f9bdff1fe08f7dc16a4d6b453da13731cf372bb3
Ray Vardaro <rvardar@uw.edu>
2024-05-28 20:02:39 -0700

    deleted:    Firstchart.R 	modified:   README.md 	deleted:    Secondchart.R 	deleted:    Thirdchart.R 	modified:   server.R 	modified:   ui.R

--- server.R
+++ server.R
@@ -15,26 +15,57 @@ library(stringr)
 
 dataset<- read.csv("dataset.csv")
 
-first_chart<- function(input, output) {
-  output$first_chart <- renderTable({
-    ggplot(dataset = dataset, aes_string(x= dataset$danceability, y= dataset$energy, color="#20C010"))+
-      geom_point()
+server <- function(input, output) {
+  output$dance_vs_energy <- renderPlot({
+    filtered_data <- dataset %>%
+      filter(danceability >= input$dance_min & danceability <= input$dance_max & energy >= input$energy_min & energy <= input$energy_max)
+    ggplot(filtered_data, aes(x = danceability, y = energy)) +
+      geom_point() +
+      labs(title = "Danceability vs. Energy",
+           x = "Danceability", y = "Energy")
   })
-  output$correlation <-first_chart
+  output$genre_select <- renderPlot({
+    genre_choices <- unique(dataset$track_genre)
+    num_genres <- input$num_genres
+    lapply(seq_len(num_genres), function(i) {
+      selectInput(paste0("genre_", i), paste0("Genre ", i), choices = genre_choices)
+    })
+  })
+  output$popularity_by_genre <- renderPlot({
+    selected_genres <- lapply(seq_len(input$num_genres), function(i)
+      input[[paste0("genre_", i)]])
+    filtered_data <- dataset %>%
+      filter(track_genre %in% selected_genres)  # Filter by selected genres
+    
+    avg_popularity <- aggregate(popularity ~ track_genre, data = filtered_data, FUN = mean)
+    
+    sum_popularity <- filtered_data %>%
+      group_by(track_genre) %>%
+      summarise(total_popularity = sum(popularity))
+    
+    combined_popularity <- left_join(avg_popularity, sum_popularity, by = "track_genre")
+    
+    ggplot(combined_popularity, aes(y = track_genre, x = popularity)) +
+      geom_bar(stat = "identity", fill = "skyblue", width = 0.7) +
+      geom_text(aes(label = track_genre), position = position_dodge(width = 0.7), vjust = -0.2, color = "black", size = 3) +
+      scale_y_discrete(limits = sort(unique(combined_popularity$track_genre), decreasing = TRUE)) +  # Sort genres alphabetically
+      theme_minimal() +
+      labs(title = "Average and Total Popularity by Genre",
+           y = "Genre",
+           x = "Popularity")
+    output$histogram <- renderPlot({
+      selected_feature <- input$feature
+      bin_width <- input$binwidth
+      
+      ggplot(dataset, aes_string(x = selected_feature)) +
+        geom_histogram(binwidth = bin_width, fill = "skyblue", color = "black", alpha = 0.7, aes(tooltip = paste(x, ..count.., sep=": "))) +  # Add tooltip aesthetics
+        labs(title = paste("Histogram of", selected_feature),
+             x = selected_feature,
+             y = "Frequency") +
+        theme_minimal()
+    })
+  })
 }
 
   
-      
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
-}
+  
--- ui.R
+++ ui.R
@@ -6,60 +6,105 @@
 #
 #    https://shiny.posit.co/
 #
-
 library(shiny)
-library(tidyverse)
+library(dplyr)
 library(ggplot2)
-library(stringr)
-
+library(readr)
+library(tidyverse)
+options(encoding = "UTF-8")
 dataset <- read.csv("dataset.csv")
-ui<- navbarPage("The Analysis of Spotify Music")
-tabPanel("Introduction")
-fluidPage (
-    titlePanel("Introduction"),
-    sidebarLayout(
+dataset
+ui<- navbarPage(
+  "The Analysis of Spotify Music",
+  tabPanel(
+    "Introduction",
+    fluidPage(
+      titlePanel("Introduction"),
+      sidebarLayout(
         sidebarPanel(
-            tags$h3("Do musical characteristics such as danceability, energy levels, and valence scores correlate to a song's popularity? This question is important because understanding the relationship between various musical aspects and track popularity might provide useful insights into listener tastes and behaviour. To address the question, we will analyze a dataset comprising information on the musical features and popularity ratings of numerous songs. We are concerned with identifying if certain musical genres yield more popular songs since it can shed light on the relationships between music genres and audience preferences, potentially altering business practices and artist strategies. To address this concern, we plan to conduct statistical analyses comparing the popularity of songs in various genres. Consider how certain musicians constantly receive greater popularity scores. This is important because it implies the presence of trends or patterns in the music industry that may inform commercial strategy or artistic decisions. Accordingly, we plan to investigate the features and attributes shared by artists with high popularity scores to better understand their impact on audience reaction.", style= "font-size: 14px; font-weight: normal; line-height:1.5;"),
-            img(src = "https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg", height= 175, width=290),
+          tags$h6("Do musical characteristics such as danceability, energy levels, and valence scores correlate to a song's popularity? This study will analyze a dataset comprising information on the musical features, popularity ratings of numerous songs, and the relationships between music genres and audience preferences. We have tied the how trends within the music industry to inform commercial strategy or artistic decisionsconduct statistical analyses comparing the popularity of songs in various genres. This is important because it implies the presence of trends or patterns in the music industry that may inform commercial strategy or artistic decisions.", style= "font-size: 14px; font-weight: normal; line-height:1.5;"),
+          img(src = "https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg", height= 175, width=290)),
         mainPanel(
-            h3("Our Final Project is based on the Spotify Tracks Dataset by the user maharshipandya. In this dataset, over 114,000 songs were collected and filtered by unique factors such as song name, artist name, popularity, song danceability and energy levels. With these various factors, our group is able to establish questions that have correlating trends from the collected data."),
-            tags$h3("Our group found the Spotify data through the Google Datasets search, which can be found here: https://huggingface.co/datasets/maharshipandya/spotify-tracks-dataset:", tags$a(href= "https://huggingface.co/datasets/maharshipandya/spotify-tracks-dataset", "https://huggingface.co/datasets/maharshipandya/spotify-tracks-dataset"),
+          h3("Our Final Project is based on the Spotify Tracks Dataset by the user maharshipandya. In this dataset, over 114,000 songs were collected and filtered by unique factors such as song name, artist name, popularity, song danceability and energy levels. With these various factors, our group is able to establish questions that have correlating trends from the collected data."),
+          tags$h5("Our group found the Spotify data through the Google Datasets search, which can be found here:", tags$a(href= "https://huggingface.co/datasets/maharshipandya/spotify-tracks-dataset", "huggingface.co/datasets/maharshipandya/spotify-tracks-dataset."),
                     "The data was collected by the user named maharshipandya and cleaned using Spotify's Web API and Python.
                     The data was initially collected to build a recommendation system based on some listener's preference and to properly establish classification purposes based on audio features and available genres."),
-           tags$h3("•	Do musical characteristics such as danceability and song energy levels correlate to popularity?"),
-           tags$h3("•	Do high valence scores relate to a song’s popularity?"),
-           tags$h3("•	Do certain musical genres produce more popular songs?"),
-           tags$h3("•	Which artists have the highest popularity scores and is there a common trend?"),
-           tags$h3("There are 114,000 rows in total."),
-           tags$h3("There are 20 columns in total."),
-           tags$h3("The possible implications of this project may affect  technologists, designers and policymakers. For technologists, the findings from the study can be used to improve algorithm recommendations. It also gives us additional information that we may use to improve the algorithm's efficiency. Understanding the links between music selections and their popularity, genre, instruments played, and other factors can also assist the algorithm improve music discovery and recommend new options. For designers, they might include the research findings into the user app interface. For example, put popular tracks first or include them in recommended albums. Designers can utilize this function to promote niche genres or styles that are underrepresented in mainstream media. Policymakers can utilize this information to understand the music industry better. This could lead to stronger business regulation and efforts to support artists through licensing and copyright. A policymaker might also implement more data-driven strategies to support budding musicians by analyzing how people react to certain tracks."),
-           tags$h3("There may be some challenges and limits with the dataset. One of them is the accuracy of the data extracted from the dataset. Any minute mistakes or omissions in recording the song's timing, or a lack of information on when it was released. Another worry is the ethical considerations around the data. We don't know if the information was given with consent or if there were any privacy or representation issues. 
-Another problem is that this dataset is based on a specific timeframe, and we must match the dataset's insight to the study questions based on that timeframe. We can't apply the same findings to current music trends because they're so dynamic. The music industry undergoes numerous changes that are difficult to predict using the data we have. We'll need more recent data to do this. We also don't know how the dataset rubrics were calculated. 
-For example, the popularity of songs we know is graded from 0 to 100, but we don't know how the figures were computed. These are some challenges and limitations we will have to address with our project.")
+          tags$h5("•	Do musical characteristics such as danceability and song energy levels correlate to popularity?"),
+          tags$h5("•	Do high valence scores relate to a song’s popularity?"),
+          tags$h5("•	Do certain musical genres produce more popular songs?"),
+          tags$h5("•	Which artists have the highest popularity scores and is there a common trend?"),
+          tags$h5("There are 114,000 rows in total."),
+          tags$h5("There are 20 columns in total."),
+          tags$h5("The possible implications of this project may affect  technologists, designers and policymakers. For technologists, the findings gives us additional information that we may use to improve the algorithm's efficiency. Understanding the links between music selections and their popularity, genre, instruments played, and other factors can also assist the algorithm improve music discovery and recommend new options. For designers, they might include the research findings into the user app interface and utilize this function to promote niche genres or styles that are underrepresented in mainstream media. Policymakers can utilize this information to understand the music industry better, which could lead to stronger business regulation and efforts to support artists through licensing and copyright."),
+          tags$h5("There may be some challenges and limits with the dataset. Any minute mistakes or omissions in recording the song's timing, or a lack of information on when it was released. Another worry is the ethical considerations around the data. We don't know if the information was given with consent or if there were any privacy or representation issues. 
+This dataset is based on a specific timeframe, and we must match the dataset's insight to the study questions based on that timeframe. The music industry undergoes numerous changes that are difficult to predict using the data we have. We will need more recent data to do this. We also don't know how the dataset rubrics were calculated. For example, the popularity of songs we know is graded from 0 to 100, but we don't know how the figures were computed.")
+          )
         )
-    )
-),
-tabPanel(
-  "First Chart",
-  fluidPage(
-    titlePanel("Song Danceability Vs Energy"),
-    sidebarLayout(
-      sidebarPanel(
-        tags$h3("The graph helps identify the relationship between the two variables: Energy and Danceability. It helps us visualize the common perception that higher energy songs are like to have high danceability. Since both the variable are numbers using a scatter plot also helps understand the relationship much better. We see from the graph that there seems to be a positive relationship between the two variables. Though we see a lot of data crowded between 0.25-0.75 of the dancebility we can assume they fall in the normal bell shaped curves. We may conclude that most of the songs have a good energy hence resulting in danceability. But we may also say that due to the song having high danceability people assume it to have high energy. Hence, we know they share a positive relationship but we cannot assume one cause the other since there are several factors that contribute into this relationship."),
+      )
+    ),
+  tabPanel(
+    "Danceability vs. Energy",
+    fluidPage(
+      titlePanel("Danceability vs. Energy"),
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput("dance_min", "Danceability Min", min = min(dataset$danceability),
+                      max = max(dataset$danceability), value = min(dataset$danceability)),
+          sliderInput("dance_max", "Danceability Max", min = min(dataset$danceability),
+                      max = max(dataset$danceability), value = max(dataset$danceability)),
+          sliderInput("energy_min", "Energy Min", min = min(dataset$energy),                       max = max(dataset$energy), value = min(dataset$energy)),
+          sliderInput("energy_max", "Energy Max", min = min(dataset$energy),
+                      max = max(dataset$energy), value = max(dataset$energy)
+          )),
         mainPanel(
-          plotOutput("first_chart"),
-          sliderInput("danceability",
-                      "Danceability:",
-                      min= 0,
-                      max = 1,
-                      value = .2),
-          sliderInput("energy",
-                      "Energy:",
-                      min = 0,
-                      max = 1,
-                      value = .2)
+          plotOutput("danceability_vs_energy")
         )
       )
     )
+  ),
+  tabPanel(
+    "Average & Total Popularity by Genre",
+    fluidPage(
+      titlePanel("Average & Total Popularity by Genre"),
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("num_genres", "Number of Genres to Compare", choices = 2:10),
+          plotOutput("genre_select")),
+        mainPanel(
+          plotOutput("popularity_by_genre")
+        )
+      )
+    )
+  ),
+  tabPanel(
+    "Audio Feature Distribution",
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          helpText("Explore the relationship of the frequency of the audio features."),
+          selectInput(
+            "feature",
+            "Select Feature:",
+            names("audio_features")
+      ),
+      sliderInput(
+        "binwidth",
+        "Bin Width:",
+        min = 0.01,
+        max = 0.5,
+        value = 0.05,
+        step = 0.01
+      )
+      ),
+      mainPanel(
+        plotOutput("histogram")
+      )
+    )
   )
-)
+))
+
+
+server<- function(input, output, session){
+}
+
+shinyApp(ui,server)
